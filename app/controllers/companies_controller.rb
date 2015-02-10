2a0a77f88ed3ee062ee92c5f244372011be8d501
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-02-10 14:27:26 -0700

    add company show view

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,4 +2,11 @@ class CompaniesController < ApplicationController
   def index
     @companies = Company.all
   end
+
+  def show
+    @company = Company.find(params[:id])
+    @locations = @company.locations
+    @products = @company.products
+  end
+
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ActiveRecord::Base
   has_many :locations
+  has_many :products
 
 end
